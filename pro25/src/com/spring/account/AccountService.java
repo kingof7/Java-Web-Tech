3c796176748fc4eed56bb1c6a2a21c7a37af7dab
jhlee <vlfl0@naver.com>
2020-12-13 20:05:52 +0900

    주석풀기, 잔액확인(유지)

--- pro25/src/com/spring/account/AccountService.java
+++ pro25/src/com/spring/account/AccountService.java
@@ -3,7 +3,7 @@ package com.spring.account;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-//@Transactional(propagation=Propagation.REQUIRED)
+@Transactional(propagation=Propagation.REQUIRED)
 public class AccountService {
 	private AccountDAO accDAO;
 
